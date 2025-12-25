f4c563633a152e54d9ffd65671d7a48960ace6a8
venonssy-creator <venonssy@gmail.com>
2025-12-24 20:41:42 -0500

    Create middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+// middleware.ts
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  if (pathname === "/") {
+    return NextResponse.rewrite(new URL("/b.html", request.url));
+  }
+
+  return NextResponse.next();
+}
